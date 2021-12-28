d2ed883483367405154efe187e8b6ce052500f3d
coehlrich <coehlrich@users.noreply.github.com>
2021-12-28 18:27:13 +1300

    Add transformer

--- src/main/java/com/wynntils/eventbustransformer/EventBusTransform.java
+++ src/main/java/com/wynntils/eventbustransformer/EventBusTransform.java
@@ -3,6 +3,7 @@ package com.wynntils.eventbustransformer;
 import dev.architectury.transformer.transformers.base.ClassEditTransformer;
 import net.minecraftforge.eventbus.EventBusEngine;
 import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.ClassNode;
 
@@ -21,6 +22,12 @@ public class EventBusTransform implements ClassEditTransformer {
             normalClassReader.accept(normalNode, 0);
 
             EventBusEngine.INSTANCE.processClass(normalNode, type);
+
+            ClassWriter normalClassWriter = new ClassWriter(0);
+            normalNode.accept(normalClassWriter);
+            dev.architectury.transformer.shadowed.impl.org.objectweb.asm.ClassReader architecturyClassReader = new dev.architectury.transformer.shadowed.impl.org.objectweb.asm.ClassReader(normalClassWriter.toByteArray());
+            node = new dev.architectury.transformer.shadowed.impl.org.objectweb.asm.tree.ClassNode();
+            architecturyClassReader.accept(node, 0);
         }
         return node;
     }
